edaa889600ed3a6072e3daf8fc45cb09a80d93a1
Kevin Taing <taingkn@gmail.com>
2022-11-23 17:16:34 +0100

    :sparkles: remove sr from ci

--- .releaserc.js
+++ .releaserc.js
@@ -6,7 +6,7 @@ const dateFormat = require('dateformat');
 const readFileAsync = promisify(require('fs').readFile);
 
 // Given a `const` variable `TEMPLATE_DIR` which points to "<semantic-release-gitmoji>/lib/assets/templates"
-const TEMPLATE_DIR = '.semantic-release-gitmoji';
+const TEMPLATE_DIR = '.semantic-release';
 // the *.hbs template and partials should be passed as strings of contents
 const template = readFileAsync(path.join(TEMPLATE_DIR, 'default-template.hbs'));
 const commitTemplate = readFileAsync(
