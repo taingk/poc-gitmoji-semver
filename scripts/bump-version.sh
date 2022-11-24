56dbc4a8c020d602b60dcb62461edc67683f0486
Kevin Taing <taingkn@gmail.com>
2022-11-24 15:48:53 +0100

    :bug: add semantic-release to devDep and yarn version

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-yarn version --new-version $1 
-yarn workspaces foreach exec yarn version --new-version $1 --no-git-tag-version
+yarn version apply --all $1
 git commit -a -m ":bookmark: bump to v${1}"
 git push
