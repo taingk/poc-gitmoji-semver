ef8a0621393af3c8a5568d89658596a0f5271ce1
Kevin Taing <taingkn@gmail.com>
2022-11-25 10:24:38 +0100

    :wrench: update changelogs

--- .releaserc.js
+++ .releaserc.js
@@ -2,12 +2,17 @@ const path = require('path');
 const { promisify } = require('util');
 const dateFormat = require('dateformat');
 const readFileAsync = promisify(require('fs').readFile);
+const { gitmojis } = require('./gitmojis.json');
 
 const TEMPLATE_DIR = '.semantic-release';
 const template = readFileAsync(path.join(TEMPLATE_DIR, 'default-template.hbs'));
 const commitTemplate = readFileAsync(
   path.join(TEMPLATE_DIR, 'commit-template.hbs')
 );
+const majorGitmojis = gitmojis.filter((gitmoji) => gitmoji.semver === 'major');
+const minorGitmojis = gitmojis.filter((gitmoji) => gitmoji.semver === 'minor');
+const patchGitmojis = gitmojis.filter((gitmoji) => gitmoji.semver === 'patch');
+const otherGitmojis = gitmojis.filter((gitmoji) => gitmoji.semver === null);
 
 module.exports = {
   branches: ['main'],
@@ -16,44 +21,9 @@ module.exports = {
       'semantic-release-gitmoji',
       {
         releaseRules: {
-          major: [':boom:'],
-          minor: [':sparkles:'],
-          patch: [
-            ':bug:',
-            ':ambulance:',
-            ':lock:',
-            ':zap:',
-            ':lipstick:',
-            ':arrow_down:',
-            ':arrow_up:',
-            ':pushpin:',
-            ':chart_with_upwards_trend:',
-            ':heavy_plus_sign:',
-            ':heavy_minus_sign:',
-            ':wrench:',
-            ':globe_with_meridians:',
-            ':pencil2:',
-            ':rewind:',
-            ':package:',
-            ':alien:',
-            ':bento:',
-            ':wheelchair:',
-            ':speech_balloon:',
-            ':card_file_box:',
-            ':children_crossing:',
-            ':iphone:',
-            ':egg:',
-            ':alembic:',
-            ':mag:',
-            ':label:',
-            ':triangular_flag_on_post:',
-            ':goal_net:',
-            ':dizzy:',
-            ':wastebasket:',
-            ':passport_control:',
-            ':adhesive_bandage:',
-            ':necktie:',
-          ],
+          major: majorGitmojis.map((gitmoji) => gitmoji.code),
+          minor: minorGitmojis.map((gitmoji) => gitmoji.code),
+          patch: patchGitmojis.map((gitmoji) => gitmoji.code),
         },
         releaseNotes: {
           template,
@@ -62,8 +32,61 @@ module.exports = {
             datetime: function (format = 'UTC:yyyy-mm-dd') {
               return dateFormat(new Date(), format);
             },
-            json: function (context) {
-              return JSON.stringify(context);
+            formattedChangelogs: function (context) {
+              const commits = Object.values(context).flat();
+              const majorCommits = commits.filter((commit) => {
+                const listMajorGitmojis = majorGitmojis.map(
+                  (gitmoji) => gitmoji.emoji
+                );
+
+                return listMajorGitmojis.includes(commit.gitmoji);
+              });
+              const minorCommits = commits.filter((commit) => {
+                const listMinorGitmojis = minorGitmojis.map(
+                  (gitmoji) => gitmoji.emoji
+                );
+
+                return listMinorGitmojis.includes(commit.gitmoji);
+              });
+              const patchCommits = commits.filter((commit) => {
+                const listPatchGitmojis = patchGitmojis.map(
+                  (gitmoji) => gitmoji.emoji
+                );
+
+                return listPatchGitmojis.includes(commit.gitmoji);
+              });
+              const otherCommits = commits.filter((commit) => {
+                const listOtherGitmojis = otherGitmojis.map(
+                  (gitmoji) => gitmoji.emoji
+                );
+
+                return listOtherGitmojis.includes(commit.gitmoji);
+              });
+
+              const formattedChangelogs = `
+              ## 💥 Breaking changes
+              ${majorCommits.map(
+                (major) =>
+                  `- [${major.commit.short}](https://github.com/${major.owner}/${major.repo}/commit/${major.commit.short}) ${major.gitmoji} ${major.subject}`
+              )}
+              ## ✨ New features
+              ${minorCommits.map(
+                (minor) =>
+                  `- [${minor.commit.short}](https://github.com/${minor.owner}/${minor.repo}/commit/${minor.commit.short}) ${minor.gitmoji} ${minor.subject}`
+              )}
+              ## 🔧 Patch changes
+              ${patchCommits.map(
+                (patch) =>
+                  `- [${patch.commit.short}](https://github.com/${patch.owner}/${patch.repo}/commit/${patch.commit.short}) ${patch.gitmoji} ${patch.subject}`
+              )}
+              ## 🚧 Others
+              ${otherCommits.map(
+                (other) =>
+                  `- [${other.commit.short}](https://github.com/${other.owner}/${other.repo}/commit/${other.commit.short}) ${other.gitmoji} ${other.subject}`
+              )}
+              `;
+
+              return formattedChangelogs;
             },
           },
           issueResolution: {
