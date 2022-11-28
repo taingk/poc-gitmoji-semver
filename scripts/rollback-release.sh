c90b13f03ec09065edfd9f3f4177755ac5f090a5
Kevin Taing <taingkn@gmail.com>
2022-11-28 17:57:21 +0100

    :bug: fix rollback-release script

--- scripts/rollback-release.sh
+++ scripts/rollback-release.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-git push --delete origin $1
-git tag --delete $1
-yarn workspaces foreach exec yarn version $1
+git push --delete origin v$1
+git tag --delete v$1
+yarn workspaces foreach exec yarn version $2
 git add ':(glob)**/package.json' .yarn/versions
-git commit -m ":bookmark: rollback to v${1}"
+git commit -m ":bookmark: rollback to v${2}"
 git push
