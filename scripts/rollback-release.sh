eb340c892b33f4823d32da1e0fe34ab7cdc81d02
Kevin Taing <taingkn@gmail.com>
2022-11-28 18:08:38 +0100

    :lock: add safety to rollback-release script

--- scripts/rollback-release.sh
+++ scripts/rollback-release.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-git push --delete origin v$1
-git tag --delete v$1
-yarn workspaces foreach exec yarn version $2
-git add ':(glob)**/package.json' .yarn/versions
-git commit -m ":bookmark: rollback to v${2}"
-git push
+echo "Input the release to delete (ex: '1.0.1'):"
+read from
+echo "Input the release to go back to (ex: '1.0.0'):"
+read to
+
+read -p "Do you want to from $from to $to? (y/n) " yn
+
+case $yn in 
+	[yY] ) echo Rollbacking...;
+    git push --delete origin v$from
+    git tag --delete v$from
+    yarn workspaces foreach exec yarn version $to
+    git add ':(glob)**/package.json' .yarn/versions
+    git commit -m ":bookmark: rollback to v${to}"
+    git push
+		break;;
+	[nN] ) echo exiting...;
+		exit;;
+	* ) echo invalid response;;
+esac
