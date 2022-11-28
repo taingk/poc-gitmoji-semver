2ff6cb034d903508f7885ba591d407ddb78bc611
Kevin Taing <taingkn@gmail.com>
2022-11-28 17:50:55 +0100

    :sparkles: add rollback-release script

--- scripts/rollback-release.sh
+++ scripts/rollback-release.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+git push --delete origin $1
+git tag --delete $1
 yarn workspaces foreach exec yarn version $1
 git add ':(glob)**/package.json' .yarn/versions
-git commit -m ":bookmark: bump to v${1}"
+git commit -m ":bookmark: rollback to v${1}"
 git push
