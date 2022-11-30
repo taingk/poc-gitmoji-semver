c1cf40d294deec25cad617b82916c454b342db1a
Kevin Taing <taingkn@gmail.com>
2022-11-30 16:13:23 +0100

    :ambulance: uppercase for commit

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -2,5 +2,5 @@
 
 yarn workspaces foreach exec yarn version $1
 git add ':(glob)**/package.json' .yarn/versions
-git commit -m ":bookmark: bump to v${1}"
+git commit -m ":bookmark: Bump to v${1}"
 git push
